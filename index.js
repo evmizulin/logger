871e9385875704d08731ed273fc7692d87c6da23
Unknown <evgeny.mizulin@gmail.com>
2017-05-15 13:53:25 +0300

    memory usage log added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+function getMemory() {
+    const memory = Math.floor(process.memoryUsage().heapUsed * 1e-6)
+    return `MemoryUsage: ${memory}Mb`
+}
+
 module.exports = function create(nameSpace) {
     return function () {
         const args = Array.prototype.map.call(arguments, item => item)
         const date = new Date()
 
         args.unshift(nameSpace)
+        args.unshift(getMemory())
         args.unshift(date.toISOString().replace('T', ' ').split('.')[0])
 
         const argsForPrint = args.map(item => {
